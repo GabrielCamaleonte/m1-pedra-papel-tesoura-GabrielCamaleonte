1ad03747e58ce26894b812e8e4248d13c41cf183
Gabriel Camaleonte <gmcamaleonte@gmail.com>
2024-03-19 17:09:50 -0300

    console.log apagado

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 /* Desenvolva seu código abaixo */
-console.log("Primeira entrega!")
+function playRockPaperScissor(player1, player2){
+    if(player1 == "Pedra" && player2 == "Tesoura" || player1 == "Tesoura" && player2 == "Papel" || player1 == "Papel" && player2 == "Pedra"){
+        console.log("jogador 1 venceu!")
+    }
+}
+console.log(playRockPaperScissor(Pedra, Pedra));
